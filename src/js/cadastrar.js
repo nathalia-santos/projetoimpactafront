68e488f416176afe6011e6a525da4ec0dfdf0bb6
nathalia-santos <nathaliasantospbg@hotmail.com>
2024-03-25 19:03:57 -0300

    first commit

---
+++ src/js/cadastrar.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+            // Seletor movido para dentro do evento DOMContentLoaded
+            const filmeForm = document.getElementById('filmeForm');
+
+            // Adiciona um ouvinte de evento para o envio do formulário
+            filmeForm.addEventListener('submit', function(event) {
+                event.preventDefault(); // Impede o envio padrão do formulário
+
+                // Obtém os elementos do formulário
+                const { nome, duracao, ano, genero } = filmeForm.elements;
+
+                // Cria o objeto filme com os valores dos campos
+                const filme = {
+                    nome: nome.value,
+                    duracao: duracao.value,
+                    ano: ano.value,
+                    genero: genero.value
+                };
+
+                console.log("Filme cadastrado:", filme);
+                
+                // Adicione aqui lógica adicional, como enviar os dados para o servidor ou limpar os campos do formulário
+                fetch('http://localhost:3000/movies')
+                    .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao fazer a requisição: ' + response.statusText);
+                        }
+                return response.json();
+                })
+            .then(data => {
+            // Faça algo com os dados recebidos do backend
+                console.log(data);
+                })
+            .catch(error => {
+    // Trate os erros que possam ocorrer durante a requisição
+            console.error('Erro:', error);
+            });
+
+            });
+        });
